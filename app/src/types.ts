906d0a3e4a13f994d520a01bde818a27deac7387
shayan.heidary <shayan.heidary@knime.com>
2024-03-01 15:14:31 +0100

    moved types to the type.ts

---
+++ app/src/types.ts
@@ -0,0 +1,19 @@
+/**
+ * Restaurant
+ */
+export type Restaurant = {
+  id: string
+  name: string
+  address: string
+  website: string
+  status: string
+}
+
+/**
+ * Dish type
+ */
+export type Dish = {
+  id: string
+  name: string
+  status: string
+}
